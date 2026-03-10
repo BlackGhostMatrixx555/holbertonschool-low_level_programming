bee67ee09ad141152b4ad9e1c57d21186b7da6bd
BlackGhostMatrixx555 <thelyaan444@hotmail.com>
2026-03-10 14:30:44 +0100

    The text show

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -1,18 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
-	char *s = "hello, world";
-	char *f = "oleh";
-	unsigned int n;
+    char *str;
 
-	n = _strspn(s, f);
-	printf("%u\n", n);
-	return (0);
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+    return (0);
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
+/* main.h */
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);  /* prototype for _putchar */
+void _puts(char *str); /* prototype for _puts */
+
+/* any other prototypes you need */
 char *_strstr(char *haystack, char *needle);
 
 #endif /* MAIN_H */
