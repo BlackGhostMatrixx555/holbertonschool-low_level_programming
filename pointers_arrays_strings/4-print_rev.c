17184ae155f983b80fdbdad1665d14e15dc0113d
BlackGhostMatrixx555 <thelyaan444@hotmail.com>
2026-03-10 14:38:24 +0100

    The text show

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,16 +6,14 @@
  */
 void print_rev(char *s)
 {
-    int len = 0;
-    int i;
+	int len = 0;
+	int i;
 
-    /* Find the length of the string */
-    while (s[len] != '\0')
-        len++;
+	while (s[len] != '\0')
+		len++;
 
-    /* Print characters from end to start */
-    for (i = len - 1; i >= 0; i--)
-        _putchar(s[i]);
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
 
-    _putchar('\n');
+	_putchar('\n');
 }
