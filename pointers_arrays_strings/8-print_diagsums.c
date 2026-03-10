d80e7d1ebbdc4b0ad47effbad068c88344598b76
BlackGhostMatrixx555 <thelyaan444@hotmail.com>
2026-03-10 15:28:35 +0100

    Calculator Core

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -2,20 +2,21 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - prints the sum of the two diagonals
- * @a: pointer to the first element of the square matrix
+ * print_diagsums - prints the sum of the two diagonals of a square matrix
+ * @a: pointer to the first element of the matrix
  * @size: size of the square matrix
  */
 void print_diagsums(int *a, int size)
 {
-    int i;
-    int sum1 = 0, sum2 = 0;
+	int i;
+	int sum1 = 0;
+	int sum2 = 0;
 
-    for (i = 0; i < size; i++)
-    {
-        sum1 += *(a + i * size + i);         /* diagonale principale */
-        sum2 += *(a + i * size + (size - 1 - i)); /* diagonale secondaire */
-    }
+	for (i = 0; i < size; i++)
+	{
+		sum1 += *(a + i * size + i);
+		sum2 += *(a + i * size + (size - 1 - i));
+	}
 
-    printf("%d, %d\n", sum1, sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
