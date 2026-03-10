4bdc95c176f5caf25663a73a2a035bbfd8aefdf5
BlackGhostMatrixx555 <thelyaan444@hotmail.com>
2026-03-10 14:45:59 +0100

    The text show

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,8 +5,10 @@
 int _putchar(char c);  /* prototype for _putchar */
 void _puts(char *str); /* prototype for _puts */
 void print_rev(char *s);    /* prototype for print_rev */
+void puts2(char *str);
 
 /* any other prototypes you need */
 char *_strstr(char *haystack, char *needle);
+char *cap_string(char *s);
 
 #endif /* MAIN_H */
